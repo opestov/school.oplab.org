a18cf1a58f8a363b3364571d15d09c98624c72f7
Oleg Pestov <oleg.pestov@gmail.com>
2014-10-10 10:23:24 +0400

    Fix path to NuGet.exe

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mono .nuget/nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
-mono .nuget/nuget.exe install FSharp.Formatting.CommandTool -OutputDirectory packages -ExcludeVersion 
-mono .nuget/nuget.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion 
+mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+mono .nuget/NuGet.exe install FSharp.Formatting.CommandTool -OutputDirectory packages -ExcludeVersion 
+mono .nuget/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion 
 
-mono .nuget/nuget.exe install GroupProgress -OutputDirectory packages -ExcludeVersion 
-mono .nuget/nuget.exe install DotLiquid -OutputDirectory packages
+mono .nuget/NuGet.exe install GroupProgress -OutputDirectory packages -ExcludeVersion 
+mono .nuget/NuGet.exe install DotLiquid -OutputDirectory packages
 
 npm install
 
