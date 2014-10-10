43f39c893bfbc72018e3774270f2ad8ced8a9602
Oleg Pestov <oleg.pestov@gmail.com>
2014-10-10 10:09:53 +0400

    Check dependencies before execution

--- build.cmd
+++ build.cmd
@@ -1,7 +1,20 @@
 @echo off
 cls
-if not exist packages\FAKE\tools\Fake.exe (
-  nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
+
+if not exist packages\FAKE\tools\FAKE.exe  (
+  .nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  .nuget\nuget.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
 )
+if not exist packages\GroupProgress  (
+  .nuget\nuget.exe install GroupProgress -OutputDirectory packages -ExcludeVersion
+)
+if not exist packages\DotLiquid*  (
+  .nuget\nuget.exe install DotLiquid -OutputDirectory packages
+)
+
+if not exist node_modules  (
+  npm init
+)
+
 packages\FAKE\tools\FAKE.exe build.fsx %*
 pause
--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-mono tools/NuGet/nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
-mono tools/NuGet/nuget.exe install FSharp.Formatting.CommandTool -OutputDirectory packages -ExcludeVersion 
-mono tools/NuGet/nuget.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion 
+mono .nuget/NuGet/nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
+mono .nuget/NuGet/nuget.exe install FSharp.Formatting.CommandTool -OutputDirectory packages -ExcludeVersion 
+mono .nuget/NuGet/nuget.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion 
+
+mono .nuget/NuGet/nuget.exe install GroupProgress -OutputDirectory packages -ExcludeVersion 
+mono .nuget/NuGet/nuget.exe install DotLiquid -OutputDirectory packages
+
+npm init
+
 mono --runtime=v4.0 packages/FAKE/tools/FAKE.exe build.fsx $@
