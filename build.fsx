2ff6ddb5a8c310fa8a9feda2eb08b5adc9091c50
Oleg Pestov <oleg.pestov@gmail.com>
2015-01-19 22:03:04 +0300

    Add practice statements.

--- build.fsx
+++ build.fsx
@@ -47,6 +47,8 @@ Target "BuildStaticPages" (fun _ ->
     buildPage "главная()" "./bricks/index/index.html" "index.html"
     buildPage "задачи()" "./bricks/ejudge/ejudge.html" "ejudge.html"
     buildPage "фмл()" "./bricks/courses/kpml.html" "kpml.html"
+
+    CopyDir bDir "./bricks/roi" (fun n -> n.EndsWith("7z") || n.EndsWith("pdf"))
     buildPage "рои(2014-2015)" "./bricks/roi/roi.html" "roi.html"
     buildOlymp ()
 
