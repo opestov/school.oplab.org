dc489f3f989769ab95d03186e619225552909778
Oleg Pestov <oleg.pestov@gmail.com>
2015-05-05 09:15:04 +0300

    Remove reference to Crimson(1415)

--- build.fsx
+++ build.fsx
@@ -68,7 +68,6 @@ Target "GulpDefault" (fun _ ->
 Target "BuildDynamicPages" (fun _ ->
     buildPage "группа(crimson, региональный этап)" "./dynamic/groups/crimson-reg.html" "crimson-reg.html"
     buildPage "группа(crimson, 1314)" "./dynamic/groups/crimson1314.html" "crimson1314.html"
-    buildPage "группа(crimson, 1415)" "./dynamic/groups/crimson1415.html" "crimson1415.html"
     buildPage "группа(gainsboro, 1415)" "./dynamic/groups/gainsboro1415.html" "gainsboro1415.html"
     buildPage "группа('Новый Уренгой', 1415)" "./dynamic/groups/nur1415.html" "nur1415.html"
     buildPage "группа(Ноябрьск)" "./dynamic/groups/noyabrsk1415.html" "noyabrsk1415.html"
